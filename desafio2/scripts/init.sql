cb50933da0fff87b2c2383da62e8dd7243b63f7a
Anderson Buffon <abuffon@gmail.com>
2022-05-05 10:22:32 -0300

    Incluído proxy reverso nginx para acesso ao app node

--- desafio2/scripts/init.sql
+++ desafio2/scripts/init.sql
@@ -1,1 +1,4 @@
 create table people(id int not null auto_increment, name varchar(255), primary key(id));
+INSERT INTO people(name) values('Anderson');
+INSERT INTO people(name) values('Eduardo');
+INSERT INTO people(name) values('Juca');
